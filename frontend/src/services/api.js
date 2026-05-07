d20b9d2f573256a65c5df6ebd27547ac676c4df2
Mahathi Revanur <rmt.mech2@gmail.com>
2026-05-07 11:53:03 +0530

    Domain change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mana-panchayat.onrender.com/api",
+  baseURL: "https://manapanchayat.com/api",
 });
 
 export default API;
