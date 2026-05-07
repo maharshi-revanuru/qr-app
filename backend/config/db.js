066dd9ec7cf137a07c3ab40f17d788b3674af924
Mahathi Revanur <rmt.mech2@gmail.com>
2026-05-07 15:48:05 +0530

    fixed registered problems

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,18 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+    });
+
     console.log("✅ MongoDB Connected");
+
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
+    console.error(
+      "❌ MongoDB Connection Failed:",
+      error.message
+    );
+
     process.exit(1);
   }
 };
