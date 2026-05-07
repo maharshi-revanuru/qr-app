26987e9b9333e9f16f5b3e43fa818f7d52a845d5
Mahathi Revanur <rmt.mech2@gmail.com>
2026-05-07 10:09:48 +0530

    backend coommit for registration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://mana-panchayat-hi2r.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
