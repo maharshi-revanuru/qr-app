0e3307f74509e8b46b7610dc9a29539d76dd2eb4
Mahathi Revanur <rmt.mech2@gmail.com>
2026-05-07 10:44:46 +0530

    api change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mana-panchayat.onrender.com",
-});
-
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
+  baseURL: "https://mana-panchayat.onrender.com/api",
 });
 
 export default API;
