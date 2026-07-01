1341d3d18d43bc44f0715aefb08d1fe462fdcd3c
Maharshi <56131713+LeaderMaharshi@users.noreply.github.com>
2026-07-01 12:32:07 +0530

    new changes for map ui another change

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -27,6 +27,22 @@ L.Icon.Default.mergeOptions({
   shadowUrl: markerShadow,
 });
 
+const DefaultIcon = L.icon({
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 
 function LocationPicker({ setLocation }) {
   useMapEvents({
